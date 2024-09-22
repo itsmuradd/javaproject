3314147d67d25fc040dd857998999f6575550c34
itsmuradd <138372292+itsmuradd@users.noreply.github.com>
2024-09-22 20:11:57 +0600

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    private String name;
+    private int id;
+
+    // Constructor
+    public Student(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{ID: " + id + ", Name: " + name + "}";
+    }
+}
+
